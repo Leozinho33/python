5518fc7fdda024cc6bd575587bffb248f4ee292b
xxx <129951174+Leozinho33@users.noreply.github.com>
2024-04-01 08:48:19 -0300

    Create senha.py

---
+++ senha.py
@@ -0,0 +1,10 @@
+def Verificar_senha (senha);
+    senha_correta = "1234"
+    if senha == senha_correta;)
+       print("Acesso permitido")
+    else:
+        print("acesso negado")
+
+        senha-usuario = input("Digite a senha ")
+
+        verificar_senha input ("Digite a senha: ")
